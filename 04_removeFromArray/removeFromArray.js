8bdc0caf1af2fdaaca6b3a9a93327cdc0eb4af94
qibin <qibinchen1994@gmail.com>
2023-01-16 21:23:11 +0100

    Fix the js file and include all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...restParams) {
+  for (let item of restParams) {
+    index = originalArray.indexOf(item);
 
+    if (index > -1) originalArray.splice(index, 1);
+  
+  }
+
+  return originalArray;
 };
 
 // Do not edit below this line
