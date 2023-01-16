0a4b526fb9e87853cace55d1bc32cfdfd684d41f
qibin <qibinchen1994@gmail.com>
2023-01-16 20:45:24 +0100

    Fix the js file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatedTimes) {
+  if (repeatedTimes < 0) return 'ERROR';
+  
+  return string.repeat(repeatedTimes);
 };
 
 // Do not edit below this line
