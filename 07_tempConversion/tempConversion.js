c12c246d26db57a5ae3e52f764574c1ea5fb504c
qibin <qibinchen1994@gmail.com>
2023-01-16 22:05:57 +0100

    Fix the js file and include all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitTemp) {
+  let celsiusTemp = (FahrenheitTemp - 32) * (5 / 9);
+  return +celsiusTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = celsiusTemp * (9 / 5) + 32;
+  return +fahrenheitTemp.toFixed(1);
 };
 
 // Do not edit below this line
