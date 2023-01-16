08d7ab5f134076a0325754ef517df31af58e7f6e
qibin <qibinchen1994@gmail.com>
2023-01-16 22:19:34 +0100

    Add a better solution using filter() method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
 const removeFromArray = function(originalArray, ...restParams) {
-  for (let item of restParams) {
-    index = originalArray.indexOf(item);
+  // for (let item of restParams) {
+  //   index = originalArray.indexOf(item);
 
-    if (index > -1) originalArray.splice(index, 1);
+  //   if (index > -1) originalArray.splice(index, 1);
   
-  }
+  // }
 
-  return originalArray;
+  // return originalArray;
+
+  return originalArray.filter(item => !restParams.includes(item));
 };
 
 // Do not edit below this line
