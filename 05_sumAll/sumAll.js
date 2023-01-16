c64a7a6ac198f2aa89fe9848e6cefc4de936f217
qibin <qibinchen1994@gmail.com>
2023-01-16 21:34:46 +0100

    Fix the js file and include all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let sum = 0;
 
+  if (start < 0 || end < 0 || typeof(start) !== 'number' || typeof(end) !== 'number')
+    return 'ERROR';
+
+  // Swap start and end if start is greater than end
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+
+  for (let i = start; i <= end; i++)
+    sum += i;
+
+  return sum;
 };
 
 // Do not edit below this line
