22121f7c752b85ae2240dbb9d684ac1ee955f1f8
qibin <qibinchen1994@gmail.com>
2023-01-16 22:27:11 +0100

    Add a simpler solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,13 @@
 const leapYears = function(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0 && year % 400 !== 0) return false;
+  // if (year % 4 === 0) {
+  //   if (year % 100 === 0 && year % 400 !== 0) return false;
     
-    return true;
-  }
+  //   return true;
+  // }
 
-  return false;
+  // return false;
+
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
